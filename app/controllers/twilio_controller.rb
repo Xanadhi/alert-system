2200fdb9f6d7d46237539f17ee87b0acad0f618e
Andy Tran <andytran@ehealthinnovation.org>
2015-11-21 13:48:47 -0500

    fixed auth tokens, also added in dummy button for sending SMS

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,26 +8,27 @@ class TwilioController < ApplicationController
 #  
 #   skip_before_action :verify_authenticity_token
 
+
   @@messages = []
 
   def index
   end
 
-  def sendsms
-    number_to_send_to = params[:number].to_i
-    message = params[:message]
-    travel_time = params[:travelTime]
+  def send_sms
+    # number_to_send_to = params[:number].to_i
+    # message = params[:body]
+    # travel_time = params[:travelTime]
     twilio_phone_number = "2892781799"
     @twilio_client = Twilio::REST::Client.new(Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token)
-    @twilio_client.messages.create(
+    @twilio_client.account.messages.create({
       :from => "+1#{twilio_phone_number}",
-      :to => number_to_send_to,
-      :body => "Hey there!"
-    )
-    # redirect_to deliveries_url
+      :to => 6477040202,
+      :body => "Testing out Twilio SMS"
+    })
+    redirect_to users_url
   end
 
-  def receivesms
+  def receive_sms
     body = params[:Body].upcase
     senderNumber = params[:From]
     # delivery = Delivery.where(["number LIKE ?", "#{senderNumber}"]).first
@@ -35,28 +36,18 @@ class TwilioController < ApplicationController
     # senderAddress = delivery.address
     twilio_phone_number = "2892781799"
       twiml = Twilio::TwiML::Response.new do |r|
-        r.Message "Thank you for letting us know, we'll make sure the driver is notified."
-        # @twilio_client = Twilio::REST::Client.new(Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token)
-        #   @twilio_client.messages.create(
-        #   :from => "+1#{twilio_phone_number}",
-        #   :to => 6044013954,
-        #   :body => "#{senderName} (#{senderAddress}) is unavailable for pick-up."
-        #   )
-        # notifyDriver("Customer is not available.")
-
-    
-      
+        r.Message "Thank you for your message <3"     
     end
     render :text => twiml.text, :content_type => "text/xml"
   end
 
-  def checkmessages
+  def check_messages
     @messages = @@messages
     #binding.pry
   end
 
   private
-  def notifyDriver(message)
+  def notify_driver(message)
     @@messages << message
     #binding.pry
   end
