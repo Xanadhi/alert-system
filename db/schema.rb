055045aa8d1e0411f61c13a9d78daaca5376b6b2
jlim <jessicalim@ehealthinnovation.org>
2015-11-21 13:14:08 -0500

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121173009) do
+ActiveRecord::Schema.define(version: 20151121174329) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -19,6 +25,11 @@ ActiveRecord::Schema.define(version: 20151121173009) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "phone_number"
+  end
+
+  create_table "weather_alerts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
